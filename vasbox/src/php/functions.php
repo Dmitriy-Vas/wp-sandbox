e8c5fcabf3ed4e9bc92bee9b1458360d347fd373
dmitriy-vas <dmitriy.vas.b@gmail.com>
2019-09-21 00:53:54 +0500

    Add basic HTML template and few theme features
    custom-logo have fixed size and default branding text
    custom-background and custom-header have default images
    editor-font-sizes have custom font presets
    folder with images are ignored because of their size

--- vasbox/src/php/functions.php
+++ vasbox/src/php/functions.php
@@ -5,8 +5,53 @@ function vasbox_setup() {
 
 	add_theme_support('title_tag');
 	add_theme_support('post-thumbnails');
-	add_theme_support('custom-logo');
-	add_theme_support('custom-background');
+	add_theme_support('custom-logo', array(
+		'height' => 90,
+		'width' => 90,
+		'header-text' => array( 'site-title', 'site-description' )
+	));
+	add_theme_support('custom-background', array(
+		'default-image' => get_template_directory_uri() . '/images/background.jpg',
+		'default-preset' => 'fill',
+		'default-position-x' => 'center',
+		'default-position-y' => 'center',
+		'default-size' => 'cover'
+	));
+	add_theme_support('custom-header', array(
+		'default-image' => get_template_directory_uri() . '/images/header.jpg',
+		'width' => 900,
+		'height' => 500,
+		'flex-width' => true,
+		'flex-height' => true,
+		'uploads' => true,
+		'video' => true
+	));
+	add_theme_support('editor-font-sizes', array(
+		array(
+			'name' => 'Small',
+			'shortName' => 'S',
+			'size' => 16,
+			'slug' => 'small'
+		),
+		array(
+			'name' => 'Normal',
+			'shortName' => 'M',
+			'size' => 22,
+			'slug' => 'normal',
+		),
+		array(
+			'name' => 'Large',
+			'shortName' => 'L',
+			'size' => 30,
+			'slug' => 'large'
+		),
+		array(
+			'name' => 'Huge',
+			'shortName' => 'XL',
+			'size' => 38,
+			'slug' => 'huge'
+		)
+	));
 
 	register_nav_menus(array(
 		'header' => __('Header', 'vasbox'),
@@ -23,3 +68,9 @@ function vasbox_scripts()
 	wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'vasbox_scripts');
+
+function vasbox_customize(WP_Customize_Manager $wp_customize)
+{
+
+}
+add_action('customize_register', 'vasbox_customize');
--- vasbox/src/php/header.php
+++ vasbox/src/php/header.php
@@ -1,2 +1,14 @@
-<?php
-wp_head();
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<header>
+</header>
